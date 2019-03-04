1f778a03063e07686570cbb9593b925c329e3df5
Njabulo <nnyanisa@retrorabbit.co.za>
2019-03-04 03:24:26 +0200

    Sum two integers

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,8 +8,15 @@ namespace HelloWorld
 {
     class Program
     {
+        public static int addNum(int a, int b)
+        {
+            return a + b;
+        }
+
         static void Main(string[] args)
         {
+            int sum = addNum(2, 3);
+            Console.WriteLine(sum);
             Console.WriteLine("Hello World");
             Console.ReadLine();
         }
